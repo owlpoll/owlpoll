d523bacb058557f5d922c64de29c04464e05c297
Amudhan Gunasekaran <agunasekaran@mobileiron.com>
2018-03-15 16:04:14 +0530

    Pluggin in logback

--- src/main/java/com/owlpoll/web/ServiceSample.java
+++ src/main/java/com/owlpoll/web/ServiceSample.java
@@ -2,5 +2,9 @@ package com.owlpoll.web;
 import org.slf4j.*;
 
 public class ServiceSample {
-    Logger logger = LoggerFactory.getLogger(ServiceSample.class);
+    private static final Logger logger = LoggerFactory.getLogger(ServiceSample.class);
+
+    public static void main(String[] args) {
+        logger.debug("Hello OwlPoll");
+    }
 }
